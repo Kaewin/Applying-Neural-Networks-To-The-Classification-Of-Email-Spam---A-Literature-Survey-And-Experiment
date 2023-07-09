5e3af10f8c7318e60780394b23eb96dfeede2ed5
Kaewin <gerbald@protonmail.com>
2023-07-09 15:12:22 -0500

    fixed bug in streamlit applet

--- app2.py
+++ app2.py
@@ -1,42 +1,135 @@
+import io
+import sys
+import nltk
+from nltk.corpus import stopwords
 import streamlit as st
 import tensorflow as tf
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from tensorflow.keras.metrics import Precision
+import email
+from email.message import EmailMessage
 
-def save_model(model, filepath):
-    tf.saved_model.save(model, filepath)
+def load_model():
+    model = model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(10, activation='relu', input_shape=(7,)),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
 
-def load_model(filepath):
-    model = tf.saved_model.load(filepath)
+    model.load_weights('model.h5')
 
     return model
 
-if __name__ == '__main__':
+def get_raw_text():
+    """Prompts the user to input the copied raw text of an email."""
 
-    filepath = 'model.savedmodel'
+    email_text = ""
 
-    model = load_model(filepath)
+    st.write("Please copy and paste the raw text of an email:")
+
+    line = st.text_input("")
+
+    email_text += line + "\n"
+
+    return email_text
+
+
+def get_eml_text(uploaded_file):
+    eml_content = uploaded_file.getvalue().decode('utf-8')
+    return eml_content
+
+def extract_features(email):
+    features = {
+        'number_of_words': len(email.split()),
+        'number_of_stop_words': len([word for word in email.split() if word in list(nltk.corpus.stopwords.words('english'))]),
+        'number_of_unique_words': len(set(email.split())),
+        'ratio_of_lowercase_to_uppercase': float(len([word for word in email.split() if word.islower()])) / len(email.split()) if len(email.split()) else 0,
+        'number_of_exclamation_points': email.count('!'),
+    }
+
+    # Tokenize the message body
+    tokens = nltk.word_tokenize(email)
+
+    # Remove stop words
+    stop_words = nltk.corpus.stopwords.words('english')
+    tokens = [token for token in tokens if token not in stop_words]
+
+    # Stem the tokens
+    stemmer = nltk.stem.PorterStemmer()
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+
+    # Lemmatize the tokens
+    lemmatizer = nltk.stem.WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+
+    features['number_of_unique_stemmed_words'] = len(set(stemmed_tokens))
+    features['number_of_lemmatized_words'] = len(set(lemmatized_tokens))
+
+    cleaned_body = ' '.join([stemmer.stem(token) for token in email.split() if token not in stop_words])
+    print('in features function', features)
+    return features
+
+def main():
+
+    model = load_model()
 
     st.write("The model has been loaded.")
 
-    st.write("What would you like to do?")
+    st.write("Welcome To My Email Spam Classifier!")
 
-    options = ["Make a prediction", "View the model's parameters"]
+    st.write("My model takes .eml files, or raw text. Which would you like to submit?")
+
+    options = [".eml file", "raw text"]
 
     choice = st.selectbox("Select an option", options)
 
-    if choice == "Make a prediction":
-        # Get the user's input
-        features = st.slider("Enter the features", 0, 1, step=0.01)
+    if choice == ".eml file":
+        uploaded_file = st.file_uploader("Upload an .eml file")
+        if uploaded_file is not None:
+            email_text = get_eml_text(uploaded_file)
+            features = extract_features(email_text)
+            # turn the features into a dataframe
+            features_df = pd.DataFrame(features, index=[0])
+            # print(features_df)
+            prediction = model.predict(features_df)
+            
+            # if prediction < 5, return "Spam"
+            # if prediction > 5, return "Not Spam"
+            if prediction > 0.5:
+                prediction = "Spam"
+            else:
+                prediction = "Not Spam"
+            
+            st.write("The prediction is:")
+            st.write(prediction)
 
-        # Make a prediction
-        prediction = model.predict([features])[0]
-
-        # Display the prediction
-        st.write(f"The prediction is: {prediction}")
-
-    elif choice == "View the model's parameters":
-        # View the model's parameters
-        for name, value in model.get_config().items():
-            st.write(f"{name}: {value}")
+    elif choice == "raw text":
+        email_text = get_raw_text()
+        features = extract_features(email_text)
+        # turn the features into a dataframe
+        features_df = pd.DataFrame(features, index=[0])
+        print(features_df)
+        prediction = model.predict(features_df)
+        
+        # if prediction < 5, return "Spam"
+        # if prediction > 5, return "Not Spam"
+        if prediction > 0.5:
+            prediction = "Spam"
+        else:
+            prediction = "Not Spam"
+        
+        st.write("The prediction is:")
+        st.write(prediction)
 
     else:
         pass
+
+if __name__ == '__main__':
+    main()
